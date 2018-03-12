9948b69d91fc50f3bd7278b86aa990a54853773c
Kameron97 <kgill2@uccsc.edu>
2018-03-12 02:54:07 -0700

    almost done w/ field.c

--- Field.c
+++ Field.c
@@ -37,16 +37,145 @@ FieldPosition FieldAt(const Field *f, uint8_t row, uint8_t col){
 
 
 FieldPosition FieldSetLocation(Field *f, uint8_t row, uint8_t col, FieldPosition p){
+    FieldPosition temp = f->field[row][col];
+    f->field[row][col] = p;
+    return temp;
 
 }
 
-
+//got help from my tutor on this function
 uint8_t FieldAddBoat(Field *f, uint8_t row, uint8_t col, BoatDirection dir, BoatType type){
+    int boatLength;
+    BoatDirection tempDir;
+    BoatType tempBoat;
 
-}
+    if( tempBoat == FIELD_BOAT_SMALL){
+        boatLength = 2;
+        tempBoat = FIELD_POSITION_SMALL_BOAT
+    }
+
+    else if (tempBoat == FIELD_BOAT_MEDIUM){
+        boatLength = 3;
+        tempBoat = FIELD_POSITION_MEDIUM_BOAT;
+    }
+    else if (tempBoat == FIELD_BOAT_LARGE){
+        boatLength = 4;
+        tempBoat = FIELD_POSITION_LARGE_BOAT;
+    }
+    else if (tempBoat == FILED_BOAT_HUGE){
+
+        boatLength = 5;
+        tempBoat = FIELD_POSITION_HUGE_BOAT;
+    }
+    else{
+        boatLength = 0;
+        return STANDARD_ERROR;
+    }
+
+
+    switch(dir){
+        case FIELD_BOAT_DIRECTION_NORTH:
+            int i = 0;
+            for (i = 0; i < boatLength; i++) {
+                if (row - i < 0) 
+                    return FALSE;
+            
+            if (f->field[row - i][col] != FIELD_POSITION_EMPTY) 
+                return FALSE;
+            
+            }
+            i = 0;
+            for (i = 0; i < boatLength; i++) {
+                f->field[row - i][col] = tempBoat;
+            }
+        case FIELD_BOAT_DIRECTION_SOUTH:
+         int i = 0;
+        for (i = 0; i < length; i++) {
+            if (row + i >= FIELD_ROWS) {
+                return FALSE;
+            }
+            if (f->field[row + i][col] != FIELD_POSITION_EMPTY) {
+                return FALSE;
+            }
+        }
+        i = 0;
+        for (i = 0; i < length; i++) {
+            f->field[row + i][col] = tempBoat;
+        }
+
+
+        case FIELD_BOAT_DIRECTION_EAST:
+        int i = 0;
+        for (i = 0; i < length; i++) {
+            if (col + i >= FIELD_COLS) {
+                return FALSE;
+            }
+            if (f->field[row][col + i] != FIELD_POSITION_EMPTY) {
+                return FALSE;
+            }
+        }
+        i = 0;
+        for (i = 0; i < length; i++) {
+            f->field[row][col + i] = tempBoat;
+        }
+
+
+
+        case FIELD_BOAT_DIRECTION_WEST:
+                int i = 0;
+        for (i = 0; i < length; i++) {
+            if (col - i < 0) {
+                return FALSE;
+            }
+            if (f->field[row][col - i] != FIELD_POSITION_EMPTY) {
+                return FALSE;
+            }
+        }
+        i = 0;
+        for (i = 0; i < length; i++) {
+            f->field[row][col - i] = tempBoat;
+        }
+        //        return TRUE;
+    
+
+
+    }
+    return TRUE;
+
+    }
+
+
+
 
 
 FieldPosition FieldRegisterEnemyAttack(Field *f, GuessData *gData){
+    int temp = FieldAt(f, gData->row, gData->col);
+
+    if (temp == FIELD_POSITION_SMALL_BOAT){
+        f->smallBoatLives--;
+        f->field[gData->row][gData->col] = FIELD_POSITION_HIT;
+        gData->hit = FIELD_POSITION_HIT;
+    }
+    else if (temp == FIELD_POSITION_MEDIUM_BOAT){
+                    f->mediumBoatLives--;
+            f->field[gData->row][gData->col] = FIELD_POSITION_HIT;
+            gData->hit = FIELD_POSITION_HIT;
+    }
+    else if ( temp == FIELD_POSITION_LARGE_BOAT){
+         f->largeBoatLives--;
+            f->field[gData->row][gData->col] = FIELD_POSITION_HIT;
+            gData->hit = FIELD_POSITION_HIT;
+    }
+    else if (temp == FIELD_POSITION_HUGE_BOAT){
+        f->hugeBoatLives--;
+            f->field[gData->row][gData->col] = FIELD_POSITION_HIT;
+            gData->hit = FIELD_POSITION_HIT;
+        }else {
+            f->field[gData->row][gData->col] = FIELD_POSITION_MISS;
+            gData->hit = FIELD_POSITION_MISS;
+        }
+
+        return temp;
 
 }
 
