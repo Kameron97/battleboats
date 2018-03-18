bd0e048854b2a2c9af01f2c5d9bbf4dec2588863
Kameron Gill <Kameron97@users.noreply.github.com>
2018-03-17 17:25:34 -0700

    Update ArtificialAgent.c

--- ArtificialAgent.c
+++ ArtificialAgent.c
@@ -38,18 +38,17 @@ static Field yourField, enemyField;
 static GuessData yourGData, enemyGData; // variables for guess data
 static NegotiationData yourNData, enemyNData; // variables for negotiation data
 static AgentState state = AGENT_STATE_GENERATE_NEG_DATA; // track state machine
-static FieldOledTurn status = FIELD_OLED_TURN_NONE; 
+static FieldOledTurn status = FIELD_OLED_TURN_NONE;
 static ProtocolParserStatus proParserStatus; // return value of ProtocolDecode()
 static TurnOrder getTurnOrder; // variable for TurnOrder from Protocol.c
 static AgentEvent getAgentEvent; // values based on return value of ProtocolDecode()
 
 static int stringLength;
-static uint8_t validNData;
-static uint8_t oppData;
-static int i;
+static uint8_t validNData; // return value of validataNData
+static uint8_t oppData; // holds opponent data
+static int i; // used for delay
 
 // **** Declare any function prototypes here ****
-
 
 /**
  * The Init() function for an Agent sets up everything necessary for an agent before the game
@@ -57,20 +56,15 @@ static int i;
  * etc. The agent can assume that stdlib's rand() function has been seeded properly in order to
  * use it safely within.
  */
-void AgentInit(void){
+void AgentInit(void)
+{
     FieldInit(&yourField, FIELD_POSITION_EMPTY);
     FieldInit(&enemyField, FIELD_POSITION_UNKNOWN);
 
-    int value = 0;
-
-    while (value != 1){
-    	FieldAddBoat(&yourField, rand()%FIELD_ROWS, rand()%FIELD_COLS, rand()%4, FIELD_BOAT_SMALL);
-    	FieldAddBoat(&yourField, rand()%FIELD_ROWS, rand()%FIELD_COLS, rand()%4, FIELD_BOAT_MEDIUM);
-    	FieldAddBoat(&yourField, rand()%FIELD_ROWS, rand()%FIELD_COLS, rand()%4, FIELD_BOAT_LARGE);
-    	FieldAddBoat(&yourField, rand()%FIELD_ROWS, rand()%FIELD_COLS, rand()%4, FIELD_BOAT_HUGE);
-
-    	value = 1;
-    }
+    FieldAddBoat(&yourField, 0, 0, FIELD_BOAT_DIRECTION_EAST, FIELD_BOAT_SMALL);
+    FieldAddBoat(&yourField, 1, 0, FIELD_BOAT_DIRECTION_EAST, FIELD_BOAT_MEDIUM);
+    FieldAddBoat(&yourField, 2, 0, FIELD_BOAT_DIRECTION_EAST, FIELD_BOAT_HUGE);
+    FieldAddBoat(&yourField, 3, 0, FIELD_BOAT_DIRECTION_EAST, FIELD_BOAT_LARGE);
 }
 
 /**
@@ -85,49 +79,36 @@ void AgentInit(void){
  *                  data.
  * @return The length of the string pointed to by outBuffer (excludes \0 character).
  */
-int AgentRun(char in, char *outBuffer){
-    
-//    if(in != NULL){
-//        // decode enemy guess data
-//        proParserStatus = ProtocolDecode(in, &enemyNData, &enemyGData);
-//        // check each ParserStatus to get AgentEvent
-//        if(proParserStatus == PROTOCOL_PARSING_FAILURE){
-//            getAgentEvent = AGENT_EVENT_MESSAGE_PARSING_FAILED;
-//        } else if (proParserStatus == PROTOCOL_WAITING){
-//            getAgentEvent = AGENT_EVENT_NONE;
-//        } else if(proParserStatus == PROTOCOL_PARSING_GOOD){
-//            getAgentEvent = AGENT_EVENT_NONE;
-//        } else if(proParserStatus == PROTOCOL_PARSED_COO_MESSAGE){
-//            getAgentEvent = AGENT_EVENT_RECEIVED_COO_MESSAGE;
-//        } else if(proParserStatus == PROTOCOL_PARSED_HIT_MESSAGE){
-//            getAgentEvent = AGENT_EVENT_RECEIVED_HIT_MESSAGE;
-//        } else if(proParserStatus == PROTOCOL_PARSED_CHA_MESSAGE){
-//            getAgentEvent = AGENT_EVENT_RECEIVED_CHA_MESSAGE;
-//        } else if(proParserStatus == PROTOCOL_PARSED_DET_MESSAGE){
-//            getAgentEvent = AGENT_EVENT_RECEIVED_DET_MESSAGE;
-//        } else {
-//            printf("ERROR\n");
-//        }
-//    }
-    
-    switch(state){
+int AgentRun(char in, char *outBuffer)
+{
+
+    if (in != NULL) {
+        // decode enemy guess data
+        proParserStatus = ProtocolDecode(in, &enemyNData, &enemyGData);
+    }
+
+    switch (state) {
     case AGENT_STATE_GENERATE_NEG_DATA:
         // arrow to SEND_DATA
         ProtocolGenerateNegotiationData(&yourNData);
         stringLength = ProtocolEncodeChaMessage(outBuffer, &yourNData);
         state = AGENT_STATE_SEND_CHALLENGE_DATA;
+        FieldOledDrawScreen(&yourField, &enemyField, status);
         return stringLength;
         break;
     case AGENT_STATE_SEND_CHALLENGE_DATA:
-        proParserStatus = ProtocolDecode(in, &enemyNData, NULL);
+        // proParserStatus = ProtocolDecode(in, &enemyNData, NULL);
         // arrow to DETERMINE_ORDER
-        if(proParserStatus == PROTOCOL_PARSED_CHA_MESSAGE){
+        if (proParserStatus == PROTOCOL_PARSED_CHA_MESSAGE) {
+            getAgentEvent = AGENT_EVENT_RECEIVED_CHA_MESSAGE;
             oppData = ProtocolValidateNegotiationData(&enemyNData);
             state = AGENT_STATE_DETERMINE_TURN_ORDER;
             stringLength = ProtocolEncodeDetMessage(outBuffer, &yourNData);
+            FieldOledDrawScreen(&yourField, &enemyField, status);
             return stringLength;
-        } else if(proParserStatus == PROTOCOL_PARSING_FAILURE) {
+        } else if (proParserStatus == PROTOCOL_PARSING_FAILURE) {
             // arrow to INVALID
+            getAgentEvent = AGENT_EVENT_MESSAGE_PARSING_FAILED;
             OledClear(OLED_COLOR_BLACK);
             OledDrawString(AGENT_ERROR_STRING_PARSING);
             OledUpdate();
@@ -136,23 +117,24 @@ int AgentRun(char in, char *outBuffer){
         }
         break;
     case AGENT_STATE_DETERMINE_TURN_ORDER:
-        proParserStatus = ProtocolDecode(in, &enemyNData, NULL);
+        // proParserStatus = ProtocolDecode(in, &enemyNData, NULL);
         // arrow to option state
-        if(proParserStatus == PROTOCOL_PARSED_DET_MESSAGE){
+        if (proParserStatus == PROTOCOL_PARSED_DET_MESSAGE) {
+            getAgentEvent = AGENT_EVENT_RECEIVED_DET_MESSAGE;
             validNData = ProtocolValidateNegotiationData(&enemyNData);
-            if(validNData == SUCCESS){
+            if (validNData == SUCCESS) {
                 getTurnOrder = ProtocolGetTurnOrder(&yourNData, &enemyNData);
                 // arrow to SEND_GUESS
-                if(getTurnOrder == TURN_ORDER_START){
+                if (getTurnOrder == TURN_ORDER_START) {
                     state = AGENT_STATE_SEND_GUESS;
                     status = FIELD_OLED_TURN_MINE;
                     FieldOledDrawScreen(&yourField, &enemyField, status);
-                } else if(getTurnOrder == TURN_ORDER_DEFER) {
+                } else if (getTurnOrder == TURN_ORDER_DEFER) {
                     // arrow to WAIT_FOR_GUESS
                     state = AGENT_STATE_WAIT_FOR_GUESS;
                     status = FIELD_OLED_TURN_THEIRS;
                     FieldOledDrawScreen(&yourField, &enemyField, status);
-                } else if(getTurnOrder == TURN_ORDER_TIE){
+                } else if (getTurnOrder == TURN_ORDER_TIE) {
                     // arrow to INVALID for TIE
                     OledClear(OLED_COLOR_BLACK);
                     OledDrawString(AGENT_ERROR_STRING_ORDERING);
@@ -162,14 +144,16 @@ int AgentRun(char in, char *outBuffer){
                 }
             } else {
                 // arrow to INVALID for Invalid Negotiation Data
+                getAgentEvent = AGENT_EVENT_MESSAGE_PARSING_FAILED;
                 OledClear(OLED_COLOR_BLACK);
                 OledDrawString(AGENT_ERROR_STRING_NEG_DATA);
                 OledUpdate();
                 state = AGENT_STATE_INVALID;
                 return STANDARD_ERROR;
             }
-        } else if(proParserStatus == PROTOCOL_PARSING_FAILURE) {
+        } else if (proParserStatus == PROTOCOL_PARSING_FAILURE) {
             // arrow to INVALID
+            getAgentEvent = AGENT_EVENT_MESSAGE_PARSING_FAILED;
             OledClear(OLED_COLOR_BLACK);
             OledDrawString(AGENT_ERROR_STRING_PARSING);
             OledUpdate();
@@ -178,74 +162,44 @@ int AgentRun(char in, char *outBuffer){
         }
         break;
     case AGENT_STATE_SEND_GUESS:
-    	// int i;
-    	
+        // int i;
+
         for (i = 0; i < (BOARD_GetPBClock() / 8); i++);
 
-        while (FieldAt(&enemyField, enemyGData.row, enemyGData.col) != FIELD_POSITION_UNKNOWN){
+        while (FieldAt(&enemyField, enemyGData.row, enemyGData.col) != FIELD_POSITION_UNKNOWN) {
 
-            enemyGData.row =  (rand() % (FIELD_ROWS));
-            enemyGData.col =  (rand() % (FIELD_ROWS));
+            enemyGData.row = (rand() % (FIELD_ROWS));
+            enemyGData.col = (rand() % (FIELD_ROWS));
         }
         state = AGENT_STATE_WAIT_FOR_HIT;
         return ProtocolEncodeCooMessage(outBuffer, &yourGData);
         break;
 
-        
-        
-        
     case AGENT_STATE_WAIT_FOR_HIT:
-    if (getAgentEvent == PROTOCOL_PARSED_HIT_MESSAGE){
-    	if(AgentGetEnemyStatus != 0){
-    		FieldUpdateKnowledge(&enemyField, &enemyGData );
-    		FieldOledDrawScreen(&yourField, &enemyField, FIELD_OLED_TURN_THEIRS);
-    		state = AGENT_STATE_WAIT_FOR_GUESS;
-    	}
-    	else{
-    		FieldOledDrawScreen(&yourField, &enemyField, FIELD_OLED_TURN_NONE);
-    		state = AGENT_STATE_WON;
-    	}
-    }
         break;
-    
-        
-        
     case AGENT_STATE_WAIT_FOR_GUESS:
-        if (getAgentEvent == PROTOCOL_PARSED_COO_MESSAGE){
-        	if (AgentGetStatus() == 0){
-        		FieldOledDrawScreen(&yourField, &enemyField, FIELD_OLED_TURN_NONE);
-        		state = AGENT_STATE_LOST;
-        	}
-        else {
-        	FieldRegisterEnemyAttack(&yourField, &enemyGData);
-        	FieldOledDrawScreen(&yourField, &enemyField, FIELD_OLED_TURN_NONE);
-        	state = AGENT_STATE_SEND_GUESS;
-        }
-        return ProtocolEncodeHitMessage(outBuffer, &enemyGData);
-    }
+        break;
 
-        break;
-        
-        
     case AGENT_STATE_INVALID:
-        return FALSE;
+        return 0;
         break;
-        
-        
+
+
     case AGENT_STATE_LOST:
-        return FALSE;
+        return 0;
         break;
-        
-        
+
+
     case AGENT_STATE_WON:
-        return FALSE;
+        return 0;
         break;
-        
-        
+
+
     default:
-        
+        return STANDARD_ERROR;
         break;
     }
+    return STANDARD_ERROR;
 }
 
 /**
@@ -259,7 +213,8 @@ int AgentRun(char in, char *outBuffer){
  * @see Field.h:FieldGetBoatStates()
  * @see Field.h:BoatStatus
  */
-uint8_t AgentGetStatus(void){
+uint8_t AgentGetStatus(void)
+{
     return FieldGetBoatStates(&yourField);
 }
 
@@ -271,6 +226,7 @@ uint8_t AgentGetStatus(void){
  * @see Field.h:FieldGetBoatStates()
  * @see Field.h:BoatStatus
  */
-uint8_t AgentGetEnemyStatus(void){
+uint8_t AgentGetEnemyStatus(void)
+{
     return FieldGetBoatStates(&enemyField);
 }
